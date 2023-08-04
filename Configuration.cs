cd3e3da14df7dd5e39ece0f7b54fcc48f74f0990
-Cyril- <59193481@qq.com>
2023-08-05 00:01:32 +0800

    1.4.4

--- Configuration.cs
+++ Configuration.cs
@@ -1,21 +1,17 @@
 ﻿using System.ComponentModel;
 using Terraria.ModLoader.Config;
 
-namespace MultipleLures
+namespace MultipleLures;
+
+public class Configuration : ModConfig
 {
+    public override ConfigScope Mode => ConfigScope.ClientSide;
 
-    [Label("$Mods.MultipleLures.Config.Label")]
-    public class Configuration : ModConfig
-    {
-        public override ConfigScope Mode => ConfigScope.ClientSide;
+    public override void OnLoaded() => MultipleLuresGlobalProj.configuration = this;
 
-        public override void OnLoaded() => MultipleLuresGlobalProj.configuration = this;
-
-        [Label("$Mods.MultipleLures.Config.LuresAmount")]
-        [Range(0, 200)]
-        [Increment(1)]
-        [DefaultValue(5)]
-        [Slider]
-        public int LuresAmount;
-    }
+    [Range(0, 200)]
+    [Increment(1)]
+    [DefaultValue(5)]
+    [Slider]
+    public int LuresAmount;
 }
